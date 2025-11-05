d68125a2a5f56eaf10460c427d9dfdb43d04a4a8
AlexJuraska <151767106+AlexJuraska@users.noreply.github.com>
2025-11-05 19:31:15 +0100

    Added a theme system

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 import App from "./App";
+import {ThemeProvider} from "@mui/material/styles";
+import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
+        <ThemeProvider theme={theme}>
             <CssBaseline />
             <BrowserRouter>
             <App />
             </BrowserRouter>
+        </ThemeProvider>
     </React.StrictMode>
 );
---
+++ src/theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#69bf97",
+            dark: "#579f7d",
+            light: "#b1ecd3",
+            contrastText: "#ffffff",
+        },
+    },
+});
+
+export default theme;
