4d4ae82f05e1ae0e2505abbcc76df55a0a5f7c45
AlexJuraska <151767106+AlexJuraska@users.noreply.github.com>
2026-03-01 01:19:49 +0100

    Added a new layout that allows for slide-out panels

--- src/layout-system/LayoutRenderer.tsx
+++ src/layout-system/LayoutRenderer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { LayoutConfig } from "./types/LayoutConfig";
 import { ComponentMap } from "./types/ComponentMap"
 import { GridLayout } from "./layouts/GridLayout";
+import {SlideLayout} from "./layouts/SlideLayout";
 
 interface Props {
     config: LayoutConfig;
@@ -12,6 +13,8 @@ export const LayoutRenderer: React.FC<Props> = ({ config, components }) => {
     switch (config.layout) {
         case "GridLayout":
             return <GridLayout config={config} components={components} />;
+        case "SlideLayout":
+            return <SlideLayout config={config} components={components} />;
         default:
             return <div>Unknown layout type: {config.layout}</div>;
     }
